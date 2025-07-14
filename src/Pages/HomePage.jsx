d839dab212d07c2c94d5a6b5790844fc9cbfe1d4
Celestial00 <sheerazalee223@gmail.com>
2025-07-14 23:32:24 +0500

    footer update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,12 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ColorfulBackground from "../Components/TopBlur";
-
+import HeroSection from "../Components/HeroSection";
 
 export default function HomePage() {
-
-
   return (
     <>
       <ColorfulBackground />
-
- 
+      <HeroSection />
     </>
   );
 }
